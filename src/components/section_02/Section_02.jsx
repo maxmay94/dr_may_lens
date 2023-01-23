b963e8f9934f0b7abb6e873a9318f08912601a07
maxmay94 <maxmay94@gmail.com>
2023-01-23 17:37:49 -0500

    update text size for larger screen

--- src/components/section_02/Section_02.jsx
+++ src/components/section_02/Section_02.jsx
@@ -33,7 +33,7 @@ function Section_02() {
 
       <div className='absolute top-0 w-screen sm:my-20 my-10'>
         <div className='pt-36'>
-          <p className='ms:text-6xl text-3xl font-black sm:py-10 py-4 text-slate-200'>
+          <p className='md:text-6xl sm:text-5xl text-3xl font-black sm:py-10 py-4 text-slate-200'>
             LENS can be useful for those suffering with...
           </p>
           <div className='bg-slate-900/50 rounded mx-auto lg:w-2/3 sm:w-3/4 w-[90%] backdrop-blur-sm p-3'>
