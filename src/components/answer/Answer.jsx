dec7a7dc0b72a88472f4141a79b431b7aea451ab
maxmay94 <maxmay94@gmail.com>
2023-01-24 11:12:37 -0500

    update answer component"

--- src/components/answer/Answer.jsx
+++ src/components/answer/Answer.jsx
@@ -1,13 +1,8 @@
-const Answer = ({answer, note, size}) => {
+const Answer = ({answer, size}) => {
   return (
     <>
       <div className={size ? 'flex mt-2 lg:pl-40 pl-5' : 'flex mt-2 pl-5'}>
         <div className="bg-slate-300 text-slate-900 m-1 my-auto p-2 drop-shadow rounded lg:ml-36">
-          { note &&
-            <div className="text-orange-700">
-              Note for Medicare patients:
-            </div>
-          }
           {answer}
         </div>
         <img 
@@ -15,6 +10,7 @@ const Answer = ({answer, note, size}) => {
           src="/images/dad_recent.jpeg"
           alt="dr_peter_may"/>
       </div>
+      <br />
     </>
   )
 }
--- src/components/faq/FAQ.jsx
+++ src/components/faq/FAQ.jsx
@@ -15,23 +15,18 @@ const FAQ = () => {
 
           <Question question={'Who Can Benefit From LENS?'}/>
           <Answer size={true} answer={'LENS is not diagnosis-based. We are treating a dysfunctional brain, one that is stuck in maladaptive brainwave patterns and the associated neurochemistry. LENS does not give one a new brain. It can only help optimize the one you have. It works best on patients who, at one point, were normal functioning in their lives, and then something (a singular episode, multiple or repeated exposures) happened to change them. Interestingly, it doesn’t seem to matter how long ago that was.'} />
-          <br />
 
           <Question question={'Do You Work With Children?'} />
           <Answer answer={'Yes. A large part of my practice is devoted to children, adolescents, and teens, largely with issues such as ADD/ADHD, anxiety, explosive behavior and anger. Interestingly, after a few sessions, most children describe themselves as happier.'} />
-          <br />
 
           <Question question={'How effective is LENS'} />
           <Answer answer={'The success rate is remarkable. Over 75% of clients benefit significantly, often noticing improvement within 6 visits! Once a full course of treatments (usually 10-20 sessions) is completed, for most people treatment is done.  And, the changes are enduring. There is no need to receive LENS on an ongoing basis.'} />
-          <br />
 
           <Question question={'How Does LENS work?'} />
           <Answer answer={'While performing a standard 21-site EEG, sophisticated computer technology and software captures and analyzes brainwaves, and coverts them into an infinitesimal electromagnetic radio wave signal that is fed back to the brain for a fraction of a second. The signal interrupts dysfunctional brainwave patterns, allowing the brain to re-boot back to normal brainwave patterns and the associated neurochemistry. It is analogous to optimizing the hardware, which makes the programs run better!'} />
-          <br />
 
           <Question question={'How Safe Is LENS?'} />
           <Answer answer={'LENS is extremely safe. In over 75,000 clients treated with LENS, there has not been a single reported case of enduring negative effects.'} />
-          <br />
         
         </div>
       </div>
